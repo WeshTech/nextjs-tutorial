f0f4080cfdcf75db1b6ecacb088741b21b9b22d8
EngineerDavid <davidwaweru2500@gmail.com>
2025-03-10 11:35:12 +0300

    chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/app/ui/global.css';
-import { inter } from './ui/fonts';
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
 
 export default function RootLayout({
   children,
@@ -8,7 +8,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className = {`${inter.className} antialiased`}>{children}</body>
+      <head>
+        <title>Acme Dashboard</title>
+        <meta name="description" content="A simple and efficient dashboard" />
+        <link rel="icon" href="/icon.jpg" />
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+
+
+
+
+
+// import { Metadata } from "next";
+// import "@/app/ui/global.css";
+// import { inter } from "./ui/fonts";
+
+// export const metadata: Metadata = {
+//   title: "Acme Dashboard",
+//   description: "A simple and efficient dashboard",
+//   icons: {
+//     icon: "/icon.jpg",
+//   },
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${inter.className} antialiased`}>{children}</body>
+//     </html>
+//   );
+// }
